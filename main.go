b2060b0ae80c139a57f7b1d3c241d19ac1ea29f2
Jakub Pravda <jakub.pravda@blindspot.ai>
2023-05-01 11:01:46 +0200

    Cloudfront and certificate management

--- main.go
+++ main.go
@@ -1,47 +1,184 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/acm"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/cloudfront"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/route53"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"fmt"
 )
 
-var nameId = "sramek-garden-store"
+var nameId = "sramek-garden-store" // TODO move to the pulumi config
+var targetDomain = "zahradnictvi-sramek.cz" // TODO move to the pulumi config
 
 func main() {
-	createS3Bucket()
-}
+	// What I'm using:
+	// -- s3 bucket
+	// -- cloudfront
+	// -- route53
+	// -- amazon certificate manager
+	
+	log.Println("Deploying infrastructure for our static websites")
 
-func createS3Bucket() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create an AWS resource (S3 Bucket)
-		bucketName := fmt.Sprintf("%s-bucket", nameId)
-		bucket, err := s3.NewBucket(ctx, bucketName, &s3.BucketArgs{
-			Website: s3.BucketWebsiteArgs{
-				IndexDocument: pulumi.String("main.html"),
-			},
-		}); handleErr(err)
+		contentBucket := createS3Bucket(ctx)
+		cdn := instantiateCloudfront(ctx, contentBucket)	
+		createAliasRecord(ctx, targetDomain ,cdn)
 
-		_, err = s3.NewBucketOwnershipControls(ctx, "ownership-controls", &s3.BucketOwnershipControlsArgs{
-			Bucket: bucket.ID(),
-			Rule: &s3.BucketOwnershipControlsRuleArgs{
-				ObjectOwnership: pulumi.String("ObjectWriter"),
-			},
-		}); handleErr(err)
-
-		// set public access to our bucket
-		publicAccessBlock, err := s3.NewBucketPublicAccessBlock(ctx, "public-access-block", &s3.BucketPublicAccessBlockArgs{
-    		Bucket:          bucket.ID(),
-    		BlockPublicAcls: pulumi.Bool(false),
-		}); handleErr(err)
-
-		_, err = filesToBucketObjects(ctx, publicAccessBlock, bucket, "./www"); handleErr(err)
-
-		// Export the name of the bucket
-		ctx.Export("bucketName", bucket.ID())
-		ctx.Export("bucketEndpoint", bucket.WebsiteEndpoint.ApplyT(func(websiteEndpoint string) (string, error) {
-    		return fmt.Sprintf("http://%v", websiteEndpoint), nil
+		// Export the pulumi outputs
+		ctx.Export("bucketName", contentBucket.ID())
+		ctx.Export("cloudfrontDomain", cdn.DomainName)
+		ctx.Export("bucketEndpoint", contentBucket.WebsiteEndpoint.ApplyT(func(websiteEndpoint string) (string, error) {
+			return fmt.Sprintf("http://%v", websiteEndpoint), nil
 		}).(pulumi.StringOutput))
 		return nil
 	})
+}
+
+func createS3Bucket(ctx *pulumi.Context) *s3.Bucket {
+	log.Println("Creating content S3 bucket")
+	
+	bucketName := fmt.Sprintf("%s-bucket", nameId)
+	bucket, err := s3.NewBucket(ctx, bucketName, &s3.BucketArgs{
+		Website: s3.BucketWebsiteArgs{
+			IndexDocument: pulumi.String("main.html"),
+		},
+	}); handleErr(err)
+	_, err = s3.NewBucketOwnershipControls(ctx, "ownership-controls", &s3.BucketOwnershipControlsArgs{
+		Bucket: bucket.ID(),
+		Rule: &s3.BucketOwnershipControlsRuleArgs{
+			ObjectOwnership: pulumi.String("ObjectWriter"),
+		},
+	}); handleErr(err)
+	// set public access to our bucket
+	publicAccessBlock, err := s3.NewBucketPublicAccessBlock(ctx, "public-access-block", &s3.BucketPublicAccessBlockArgs{
+    	Bucket:          bucket.ID(),
+    	BlockPublicAcls: pulumi.Bool(false),
+	}); handleErr(err)
+	// create S3 buckets with web content
+	_, err = filesToBucketObjects(ctx, publicAccessBlock, bucket, "./www"); handleErr(err)
+	return bucket
+}
+
+func getArnCertificate(ctx *pulumi.Context) pulumi.StringOutput {
+	eastRegion, err := aws.NewProvider(ctx, "east", &aws.ProviderArgs{
+		Region: pulumi.String("us-east-1"), // AWS Certificate Manager is not available in other regions
+	})
+
+	// generate certificate for our domain
+	certificate, err := acm.NewCertificate(ctx, "certificate", &acm.CertificateArgs{
+		DomainName: pulumi.String(targetDomain),
+		ValidationMethod: pulumi.String("DNS"),
+	}, pulumi.Provider(eastRegion)); handleErr(err)
+
+	zoneId, err := getOrCreateRoute53HostedZone(ctx, targetDomain); handleErr(err)
+	log.Printf("DNS Hosted zone: %s", zoneId)
+
+	// Create a DNS record to prove we own the domain
+	certValidationDomain, err := route53.NewRecord(ctx, fmt.Sprintf("%s-validation", targetDomain), &route53.RecordArgs{
+		Name: certificate.DomainValidationOptions.ApplyT(func(options []acm.CertificateDomainValidationOption) string {
+			resourceRecordName := options[0].ResourceRecordName
+			log.Printf("DNS resource record name: %v", resourceRecordName)
+			return *resourceRecordName
+		}).(pulumi.StringOutput),
+		Type: certificate.DomainValidationOptions.ApplyT(func(options []acm.CertificateDomainValidationOption) string {
+			resourceRecordType := options[0].ResourceRecordType
+			log.Printf("DNS resource record type: %v", resourceRecordType)
+			return *resourceRecordType
+		}).(pulumi.StringOutput),
+		Records: pulumi.StringArray{ 
+			certificate.DomainValidationOptions.ApplyT(func(options []acm.CertificateDomainValidationOption) string {
+				recordValue := options[0].ResourceRecordValue
+				log.Printf("DNS record value: %v", recordValue)
+				return *recordValue
+			}).(pulumi.StringOutput)},
+		ZoneId: pulumi.String(zoneId),
+		Ttl: pulumi.Int(60),
+		}); handleErr(err)
+
+	certValidation, err := acm.NewCertificateValidation(ctx, "certificate-validation", &acm.CertificateValidationArgs{
+		CertificateArn: certificate.Arn,
+		ValidationRecordFqdns: pulumi.StringArray{ certValidationDomain.Fqdn },
+	}, pulumi.Provider(eastRegion)); handleErr(err)
+
+	return certValidation.CertificateArn
+}
+
+func instantiateCloudfront(ctx *pulumi.Context, contentBucket *s3.Bucket) *cloudfront.Distribution {
+	log.Println("Creating Cloudfront distribution")
+	
+
+	logsBucket, err := s3.NewBucket(ctx, "requests-logs", &s3.BucketArgs{
+		Acl: pulumi.String("private"),
+		Bucket: pulumi.String(fmt.Sprintf("%s-logs", targetDomain)),
+	}); handleErr(err)
+
+	_, er := s3.NewBucketOwnershipControls(ctx, "logs-ownership-controls", &s3.BucketOwnershipControlsArgs{
+		Bucket: logsBucket.ID(),
+		Rule: &s3.BucketOwnershipControlsRuleArgs{
+			ObjectOwnership: pulumi.String("BucketOwnerPreferred"),
+		},
+	}); handleErr(er)
+
+	distribution, err := cloudfront.NewDistribution(ctx, "cdn", &cloudfront.DistributionArgs {
+		Enabled: pulumi.Bool(true),
+		Aliases: pulumi.StringArray { pulumi.String(targetDomain) },
+		DefaultRootObject: pulumi.String("main.html"),
+		DefaultCacheBehavior: cloudfront.DistributionDefaultCacheBehaviorArgs {
+			TargetOriginId: contentBucket.Arn,
+			ViewerProtocolPolicy: pulumi.String("redirect-to-https"),
+			AllowedMethods: pulumi.StringArray { pulumi.String("GET"), pulumi.String("HEAD") },
+			CachedMethods: pulumi.StringArray { pulumi.String("GET"), pulumi.String("HEAD") },
+			ForwardedValues: cloudfront.DistributionDefaultCacheBehaviorForwardedValuesArgs {
+				Cookies: cloudfront.DistributionDefaultCacheBehaviorForwardedValuesCookiesArgs {
+					Forward: pulumi.String("none"),
+				},
+				QueryString: pulumi.Bool(false),
+			},
+			MinTtl: pulumi.Int(0),
+			MaxTtl: pulumi.Int(60 * 10), // 10 minutes
+			DefaultTtl: pulumi.Int(60 * 10), // 10 minutes
+		},
+		Origins: cloudfront.DistributionOriginArray {
+			cloudfront.DistributionOriginArgs {
+				OriginId: contentBucket.Arn,
+				DomainName: contentBucket.WebsiteEndpoint,
+				CustomOriginConfig: cloudfront.DistributionOriginCustomOriginConfigArgs {
+					OriginProtocolPolicy: pulumi.String("http-only"),
+					HttpPort: pulumi.Int(80),
+					HttpsPort: pulumi.Int(443),
+					OriginSslProtocols: pulumi.StringArray { pulumi.String("TLSv1.2") },
+				},
+			},
+		},
+		PriceClass: pulumi.String("PriceClass_100"),
+		
+		// Put access logs to the bucket we created before
+		LoggingConfig: cloudfront.DistributionLoggingConfigArgs {
+			Bucket: logsBucket.BucketDomainName,
+			IncludeCookies: pulumi.Bool(false),
+			Prefix: pulumi.String(fmt.Sprintf("%s/", targetDomain)),
+		},
+
+		// Set restrictions for our websites, at this moment we don't need any
+		Restrictions: cloudfront.DistributionRestrictionsArgs {
+			GeoRestriction: cloudfront.DistributionRestrictionsGeoRestrictionArgs {
+				RestrictionType: pulumi.String("none"),
+			},
+		},
+		// It takes around 15min to create cloudfront distribution, so we don't want to wait for it
+		
+		// Use the distribution certificate
+		ViewerCertificate: cloudfront.DistributionViewerCertificateArgs {
+			AcmCertificateArn: getArnCertificate(ctx),
+			SslSupportMethod: pulumi.String("sni-only"),
+		},
+		
+		WaitForDeployment: pulumi.Bool(false),
+	}); handleErr(err)
+	return distribution 
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/cloudfront"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/route53"
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"os"
@@ -37,6 +39,7 @@ func filesToBucketObjects(ctx *pulumi.Context, accessBlock *s3.BucketPublicAcces
 
 func bucketObjectConverter(ctx *pulumi.Context, accessBlock *s3.BucketPublicAccessBlock, bucket *s3.Bucket, path string) (*s3.BucketObject, error) {
 	mimeType := mime.TypeByExtension(filepath.Ext(path))
+	// remove wwwDir from the path (as we want to save files directly to the bucket root)
 	dstFilePath := strings.Replace(path, wwwDir, "", 1)
 
 	log.Printf("Converting file %s to bucket object with mime type %s\n", path, mimeType)
@@ -47,4 +50,56 @@ func bucketObjectConverter(ctx *pulumi.Context, accessBlock *s3.BucketPublicAcce
 		Source: 		pulumi.NewFileAsset(path),
 		ContentType: 	pulumi.String(mimeType),
 	}, pulumi.DependsOn([]pulumi.Resource{accessBlock}))
+}
+
+func getOrCreateRoute53HostedZone(ctx *pulumi.Context, targetDomain string) (string, error) {
+	// returns new or existing hosted zone ID
+	log.Printf("Looking up hosted zone for domain %s\n", targetDomain)
+	zoneLookupFunc := func (domain string) (*route53.LookupZoneResult, error) {
+		return route53.LookupZone(ctx, &route53.LookupZoneArgs{
+			Name: &domain,
+		})
+	}
+	
+	//zoneCreateFunc := func (domain string) (*route53.Zone, error) {
+	//	return route53.NewZone(ctx, domain, &route53.ZoneArgs{
+	//		Name: pulumi.String(domain),
+	//		Comment: pulumi.String("Hosted zone for " + domain),
+	//	})
+	//}
+	
+	lookupResult, err := zoneLookupFunc(targetDomain); handleErr(err)
+	return lookupResult.Id, nil
+
+	//if zoneLookupFuncResult, err := zoneLookupFunc(targetDomain); err != nil {
+	//	if strings.Contains(err.Error(), "no matching Route53Zone found") {
+	//		log.Print("Hosted zone not found, creating new one")
+	//		if zoneCreateFuncResult, err := zoneCreateFunc(targetDomain); err != nil {
+	//			return "", err
+	//		} else {
+	//			return zoneCreateFuncResult.ZoneId, nil
+	//		}
+	//	} else {
+	//		return "", err
+	//	}
+	//} else {
+	//	return zoneLookupFuncResult.Id, nil
+	//}
+}
+
+func createAliasRecord(ctx *pulumi.Context, domain string, distribution *cloudfront.Distribution) {
+	hzid, err := getOrCreateRoute53HostedZone(ctx, domain); handleErr(err)
+	record, err := route53.NewRecord(ctx, "www", &route53.RecordArgs{
+		Name: pulumi.String(domain),
+		ZoneId: pulumi.String(hzid),
+		Type: pulumi.String("A"),
+		Aliases: route53.RecordAliasArray{
+			&route53.RecordAliasArgs{
+				Name: distribution.DomainName,
+				ZoneId: distribution.HostedZoneId,
+				EvaluateTargetHealth: pulumi.Bool(true),
+			},
+		},
+	}); handleErr(err)
+	log.Printf("Created alias record %v for domain %s\n", record.Name, domain)
 }
