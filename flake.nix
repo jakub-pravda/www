38c0552795141a81e41e5d4018111b566aa8ff61
Jakub Pravda <me@jakubpravda.net>
2023-09-11 21:40:51 +0200

    Devshell added

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "DevShell for the WWW project";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+        };
+    });
+}
