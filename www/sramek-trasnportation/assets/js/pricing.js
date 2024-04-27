1c7e8166757904c22c90ae81d1beef4c7f2b9e4d
Jakub Pravda <me@jakubpravda.net>
2024-04-27 17:19:09 +0200

    Landfill section content

--- www/sramek-trasnportation/assets/js/pricing.js
+++ www/sramek-trasnportation/assets/js/pricing.js
@@ -2,8 +2,8 @@ fetch('assets/pricing.json')
   .then(response => response.json())
   .then(data => {
     for (let key in data.landfill) {
-      document.querySelector(`#${key}Name`).textContent = data.landfill[key].name;
-      document.querySelector(`#${key}Price`).textContent = data.landfill[key].price;
+      document.querySelector(`#${key}-name`).textContent = data.landfill[key].name;
+      document.querySelector(`#${key}-price`).textContent = data.landfill[key].price;
     }
   })
   .catch(error => console.error('Error:', error));
