00bd1a36f88a448ab787eecca0c14733f356dc32
Jakub Pravda <me@jakubpravda.net>
2024-12-08 22:45:11 +0100

    infra - move lambdas together

--- infra/src/lambda.go
+++ infra/src/lambda.go
@@ -110,6 +110,140 @@ func lambdaRedirect(ctx *pulumi.Context) *lambda.Function {
 	return lambdaFunction
 }
 
+func lambdaEmailForm(ctx *pulumi.Context) *lambda.Function {
+	lambdaArchive := "../dist/lambda_send_mail.zip"
+
+	assumeRole, err := iam.GetPolicyDocument(ctx, &iam.GetPolicyDocumentArgs{
+		Statements: []iam.GetPolicyDocumentStatement{
+			{
+				Effect: pulumi.StringRef("Allow"),
+				Principals: []iam.GetPolicyDocumentStatementPrincipal{
+					{
+						Type: "Service",
+						Identifiers: []string{
+							"lambda.amazonaws.com",
+						},
+					},
+				},
+				Actions: []string{
+					"sts:AssumeRole",
+				},
+			},
+		},
+	}, nil)
+	handleErr(err)
+
+	log.Println("Creating IAM for sending mail lambda")
+	iamForLambda, err := iam.NewRole(ctx, "iam_for_lambda", &iam.RoleArgs{
+		Name:             pulumi.String("iam_for_lambda"),
+		AssumeRolePolicy: pulumi.String(assumeRole.Json),
+	})
+	handleErr(err)
+
+	log.Println("Updating lambda policies to allow sending mails")
+
+	sesPolicy, err := iam.NewPolicy(ctx, "ses_policy", &iam.PolicyArgs{
+		Description: pulumi.String("Policy to allow sending mails through lambda"),
+		Policy: pulumi.String(`{
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "ses:SendEmail",
+                        "ses:SendRawEmail"
+                    ],
+                    "Resource": "*"
+                }
+            ]
+        }`),
+	})
+	handleErr(err)
+
+	_, err = iam.NewRolePolicyAttachment(ctx, "ses_policy_attachment", &iam.RolePolicyAttachmentArgs{
+		Role:      iamForLambda.Name,
+		PolicyArn: sesPolicy.Arn,
+	})
+	handleErr(err)
+
+	log.Println("Archiving send_mail.js lambda")
+	lambda_lookup_file, err := archive.LookupFile(ctx, &archive.LookupFileArgs{
+		Type:       "zip",
+		SourceFile: pulumi.StringRef("./lambda/lambda_send_mail.mjs"),
+		OutputPath: lambdaArchive,
+	}, nil)
+	handleErr(err)
+
+	lambdaLogging := lambdaLogs(ctx, iamForLambda, "email_form")
+
+	// Create the Lambda Function itself
+	log.Println("Creating email_form lambda")
+	lambdaFunction, err := lambda.NewFunction(ctx, "email_form", &lambda.FunctionArgs{
+		Code:           pulumi.NewFileArchive(lambdaArchive),
+		Name:           pulumi.String("lambda-email-form"),
+		Role:           iamForLambda.Arn,
+		Handler:        pulumi.String("lambda_send_mail.handler"),
+		SourceCodeHash: pulumi.String(lambda_lookup_file.OutputBase64sha256),
+		Runtime:        pulumi.String(lambda.RuntimeNodeJS18dX),
+	}, pulumi.DependsOn([]pulumi.Resource{lambdaLogging}))
+
+	if err != nil {
+		handleErr(err)
+	}
+	return lambdaFunction
+}
+
+func lambdaEmailFormCors(ctx *pulumi.Context) *lambda.Function {
+	lambdaArchive := "../dist/lambda_cors.zip"
+
+	assumeRole, err := iam.GetPolicyDocument(ctx, &iam.GetPolicyDocumentArgs{
+		Statements: []iam.GetPolicyDocumentStatement{
+			{
+				Effect: pulumi.StringRef("Allow"),
+				Principals: []iam.GetPolicyDocumentStatementPrincipal{
+					{
+						Type: "Service",
+						Identifiers: []string{
+							"lambda.amazonaws.com",
+						},
+					},
+				},
+				Actions: []string{
+					"sts:AssumeRole",
+				},
+			},
+		},
+	}, nil)
+	handleErr(err)
+
+	log.Println("Creating IAM for cors lambda")
+	iamForLambda, err := iam.NewRole(ctx, "lambda_cors_iam", &iam.RoleArgs{
+		Name:             pulumi.String("lambda_cors_iam"),
+		AssumeRolePolicy: pulumi.String(assumeRole.Json),
+	})
+	handleErr(err)
+
+	log.Println("Archiving cors.mjs lambda")
+	lambda_lookup_file, err := archive.LookupFile(ctx, &archive.LookupFileArgs{
+		Type:       "zip",
+		SourceFile: pulumi.StringRef("./lambda/lambda_cors.mjs"),
+		OutputPath: lambdaArchive,
+	}, nil)
+	handleErr(err)
+
+	log.Println("Creating cors lambda")
+	lambdaFunction, err := lambda.NewFunction(ctx, "cors", &lambda.FunctionArgs{
+		Code:           pulumi.NewFileArchive(lambdaArchive),
+		Name:           pulumi.String("lambda-cors"),
+		Role:           iamForLambda.Arn,
+		Handler:        pulumi.String("lambda_cors.handler"),
+		SourceCodeHash: pulumi.String(lambda_lookup_file.OutputBase64sha256),
+		Runtime:        pulumi.String(lambda.RuntimeNodeJS18dX),
+	})
+	handleErr(err)
+	return lambdaFunction
+}
+
 func lambdaLogs(ctx *pulumi.Context, iamRole *iam.Role, name string) *iam.RolePolicyAttachment {
 	// Create log group for lambda
 	lambdaLoggingPolicyDocument, err := iam.GetPolicyDocument(ctx, &iam.GetPolicyDocumentArgs{
