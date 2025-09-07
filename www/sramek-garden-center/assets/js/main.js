3514c782078bdca5d929e06f75afd451cb00399a
Jakub Pravda <me@jakubpravda.net>
2025-09-07 14:13:09 +0200

    WWW gardencenter - use npm and parcel for web distribution

--- www/sramek-garden-center/assets/js/main.js
+++ www/sramek-garden-center/assets/js/main.js
@@ -1,16 +1,16 @@
-/*
-	Spectral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+import $ from "jquery";
+window.$ = window.jQuery = $;
+
+import "jquery.scrollex";
+import "./jquery.scrolly.min.js";
 
 /**
  * Header elements
  */
 
 class Header extends HTMLElement {
-	/*remark: this header element must be defined before the main fuction! Main function works with the #menu element, which have to be properly inserted before!*/ 
-	connectedCallback() {
+  /*remark: this header element must be defined before the main fuction! Main function works with the #menu element, which have to be properly inserted before!*/
+  connectedCallback() {
     this.innerHTML = `
 		<nav id="nav">
 			<ul>
@@ -35,7 +35,7 @@ class Header extends HTMLElement {
 					<a href="udrzby.html">Údržby zahrad</a>
 					</li>
 					<li>
-					<a href="#three">Kontakt</a>
+					<a href="index.html#three">Kontakt</a>
 					</li>
 				</ul>
 				</div>
@@ -46,88 +46,87 @@ class Header extends HTMLElement {
   }
 }
 
-customElements.define('nav-placeholder', Header);
+customElements.define("nav-placeholder", Header);
 
-(function($) {
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $wrapper = $("#page-wrapper"),
+    $banner = $("#banner"),
+    $header = $("#header");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$wrapper = $('#page-wrapper'),
-		$banner = $('#banner'),
-		$header = $('#header');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: [null, "480px"],
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Mobile?
+  if (browser.mobile) $body.addClass("is-mobile");
+  else {
+    breakpoints.on(">medium", function () {
+      $body.removeClass("is-mobile");
+    });
 
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
-		else {
+    breakpoints.on("<=medium", function () {
+      $body.addClass("is-mobile");
+    });
+  }
 
-			breakpoints.on('>medium', function() {
-				$body.removeClass('is-mobile');
-			});
+  // Scrolly.
+  $(".scrolly").scrolly({
+    speed: 1500,
+    offset: $header.outerHeight(),
+  });
 
-			breakpoints.on('<=medium', function() {
-				$body.addClass('is-mobile');
-			});
+  // Menu.
+  $("#menu")
+    .append('<a href="#menu" class="close"></a>')
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: "right",
+      target: $body,
+      visibleClass: "is-menu-visible",
+    });
 
-		}
+  // Header.
+  if ($banner.length > 0 && $header.hasClass("alt")) {
+    $window.on("resize", function () {
+      $window.trigger("scroll");
+    });
 
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500,
-				offset: $header.outerHeight()
-			});
+    $banner.scrollex({
+      bottom: $header.outerHeight() + 1,
+      terminate: function () {
+        $header.removeClass("alt");
+      },
+      enter: function () {
+        $header.addClass("alt");
+      },
+      leave: function () {
+        $header.removeClass("alt");
+      },
+    });
+  }
 
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
-
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
-
-			$window.on('resize', function() { $window.trigger('scroll'); });
-
-			$banner.scrollex({
-				bottom:		$header.outerHeight() + 1,
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
-
-		}
-
-	// Initiate glightbox
-		const glightbox = GLightbox({
-			selector: '.glightbox',
-			preload: false
-		});
-
+  // Initiate glightbox
+  const glightbox = GLightbox({
+    selector: ".glightbox",
+    preload: false,
+  });
 })(jQuery);
